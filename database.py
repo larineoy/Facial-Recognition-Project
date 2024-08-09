27bd56f90b0ee8938dc5129c060a8b2626b53e46
larineoy <larineoyhan@gmail.com>
2024-08-08 21:59:17 -0400

    added database.py

--- database.py
+++ database.py
@@ -8,3 +8,23 @@ from utils import get_detection_boxes, facenet
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from facenet_models import FacenetModel
+
+class Database:
+    def __init__(self):
+        # self.data = None
+        # self.data = np.ndarray()
+        self.data: Dict[str, Profile] = {}
+        pass
+
+    def __repr__(self):
+        # makes printing the database more informative
+        return f"Database({repr(self.data)})"
+
+    def save_db(self, path):
+        with open(path, mode="wb") as file:
+            pickle.dump(self.data, file)
+
+    def load_db(self, path):
+        with open(path, "rb") as file:
+            self.data = pickle.load(file)
+
