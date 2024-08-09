304d0124844f34144f02224cab0153ec7a24f375
larineoy <larineoyhan@gmail.com>
2024-08-08 21:58:42 -0400

    added database.py

---
+++ database.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pickle
+import utils
+from typing import Dict
+from project_class import Profile
+from typing import Optional
+from utils import get_detection_boxes, facenet
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from facenet_models import FacenetModel
