732042c922f3566d38d78b2e185fd30dea23809f
larineoy <larineoyhan@gmail.com>
2024-08-10 00:22:21 -0400

    updated utils.py

--- utils.py
+++ utils.py
@@ -3,3 +3,11 @@ import skimage.io as io  # type: ignore
 from facenet_models import FacenetModel
 from pathlib import Path
 from typing import Union
+
+facenet = FacenetModel()
+
+def load_image(path):
+    if isinstance(path, str):
+        path = Path(path)
+    array = io.imread(str(path))
+    return array
