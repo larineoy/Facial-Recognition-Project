8ab3feff3de83f4f8cd4e5b93259edbae115d9de
larineoy <larineoyhan@gmail.com>
2024-08-11 21:06:06 -0400

    updates

--- project.py
+++ project.py
@@ -1,2 +1,9 @@
 import numpy as np
 from typing import Optional
+
+class Profile:
+    "Profile information is saved to database."
+
+    def __init__(self, name: str):
+        self.name = name
+        self.desc: Optional[np.ndarray] = None
