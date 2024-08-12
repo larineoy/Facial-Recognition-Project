2b8be4ccacf0ce9d85e321f9793eafc0b9bbdc75
larineoy <larineoyhan@gmail.com>
2024-08-11 21:06:39 -0400

    updates

--- project.py
+++ project.py
@@ -7,3 +7,10 @@ class Profile:
     def __init__(self, name: str):
         self.name = name
         self.desc: Optional[np.ndarray] = None
+
+    def mean_desc(self) -> np.ndarray:
+        if self.desc is not None:
+            return self.desc.mean(axis=0)
+        else:
+            raise ValueError("No descriptors available.")
+
