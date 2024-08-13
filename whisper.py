3e3b87785e798044ad8dec333d30d5e6e6305ebd
larineoy <larineoyhan@gmail.com>
2024-08-12 21:57:14 -0400

    updates

--- whisper.py
+++ whisper.py
@@ -3,4 +3,52 @@ from typing import NamedTuple, Optional, Sequence, Union, DefaultDict, List
 
 import numpy as np
 
+class Node:
+    """Describes a node in a graph, and the edges connected
+    to that node."""
 
+    def __init__(
+        self,
+        ID: int,
+        neighbors: Sequence[int],
+        descriptor: np.ndarray,
+        truth: Optional[str] = None,
+        file_path: Optional[str] = None,
+    ):
+        """
+        Parameters
+        ----------
+        ID : int
+            A unique identifier for this node. Should be a
+            value in [0, N-1], if there are N nodes in total.
+
+        neighbors : Sequence[int]
+            The node-IDs of the neighbors of this node.
+
+        descriptor : numpy.ndarray
+            The shape-(512,) descriptor vector for the face that this node corresponds to.
+
+        truth : Optional[str]
+            If you have truth data, for checking your clustering algorithm,
+            you can include the label to check your clusters at the end.
+            If this node corresponds to a picture of Ryan, this truth
+            value can just be "Ryan"
+
+        file_path : Optional[str]
+            The file path of the image corresponding to this node, so
+            that you can sort the photos after you run your clustering
+            algorithm
+        """
+        self.id = ID  # a unique identified for this node - this should never change
+
+        # The node's label is initialized with the node's ID value at first,
+        # this label is then updated during the whispers algorithm
+        self.label = ID
+
+        # (n1_ID, n2_ID, ...)
+        # The IDs of this nodes neighbors. Empty if no neighbors
+        self.neighbors = tuple(neighbors)
+        self.descriptor = descriptor
+
+        self.truth = truth
+        self.file_path = Path(file_path) if file_path is not None else None
