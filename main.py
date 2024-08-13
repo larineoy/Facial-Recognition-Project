b6f0589a4ff21158527746e9afdf8900a7d68c2f
larineoy <larineoyhan@gmail.com>
2024-08-12 21:59:09 -0400

    updates

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ database = db.Database()
 group_pic = list(Path("./pics/group/").glob("*.jpg"))[0]
 duncan_pics = list(Path("./pics/TimDuncan/").glob("*.jpg"))
 parker_pics = list(Path("./pics/TonyParker/").glob("*.jpg"))
+
+for picture_path in duncan_pics:
+    image = load_image(picture_path)
+    database.add_image_to_database("Tim Duncan", image)
+
+for pic_path in parker_pics:
+    img = load_image(pic_path)
+    database.add_image_to_database("Tony Parker", img)
+
+img = load_image(group_pic)
+plot_image_with_detections(img, database)
